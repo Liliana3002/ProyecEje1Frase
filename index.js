7ce2aa99c98bea15a07d481625c37e89926d3217
Liliana <93170793+Liliana3002@users.noreply.github.com>
2022-12-28 20:56:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+/* Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.*/
+const frase = prompt ('Escriba una frase')
+console.log ('La frase escrita por el usuario es:' +' '+ frase)
